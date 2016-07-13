1d2ee5892ccde7f7a5a1b37700ad785659598df5
melkotoury <melkotoury@gmail.com>
2016-07-13 21:52:41 +0200

    added the app logo , added half the questions

--- js/script.js
+++ js/script.js
@@ -53,6 +53,203 @@ $('#remove').click(function(){
 
 
 // Questions
+var currentQuestion=null;
+var questionIndex=0;
+
+// Your list of questions. Each question has an answer (either a,b,c or d)
+// and then a set of "options" in the question
+
+var questions = [
+    // question 1
+    {
+        'answer' : 'b',
+        'question' : 'مين صاحب هدف مصر الاوحد فى كاس العالم؟',
+        'options' : ['كابتن مجدى'
+        ,'كابتن مجدى عبد الغنى'
+        ,'كابتن ماجد ..عبد الغنى'
+        ,'كل ماسبق']
+    },
+    //end question 1
+
+        // question 2
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['','','','']
+    },
+    //end question 2
+          // question 3
+    {
+        'answer' : 'عدالة السماء هبطت فين ؟',
+        'question' : 'c',
+        'options' : ['ستاد المحلة'
+        ,'علي دماغ مجدي عبد العني'
+        ,'ستاد باليرمو'
+        ,'ستاد النيل']
+    },
+    //end question 3
+          // question 4
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['',
+        '',
+        '',
+        '']
+    },
+    //end question 4
+          // question 5
+    {
+        'answer' : 'b',
+        'question' : 'كابتن مدحت شلبى بيصبغ شعره فين؟',
+        'options' : ['كوافير محمد الصغير',
+        'هو اللى بيسبغ لنفسه',
+        'عند كابتن ايمن يونس',
+        'مش بيسبغ اصلا']
+    },
+    //end question 5
+          // question 6
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['',
+        '',
+        '',
+        '']
+    },
+    //end question 6
+          // question 7
+    {
+        'answer' : 'd',
+        'question' : 'ايه النادى الوحيد اللى مدربوش كابتن فاروق جعفر',
+        'options' : ['ريال مدريد',
+        'اليوفينتوس',
+        'تشيلسى',
+        'مياه البحيره']
+    },
+    //end question 7
+          // question 8
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['',
+        '',
+        '',
+        '']
+    },
+    //end question 8
+          // question 9
+    {
+        'answer' : 'd',
+        'question' : 'احرف لاعب فى تاريخ الزمالك',
+        'options' : ['عمرو الصفتى',
+        'احمد غانم سلطان',
+        'حمادة طلبه',
+        ' حازم امام الصغير']
+    },
+    //end question 9
+          // question 10
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['',
+        '',
+        '',
+        '']
+    },
+    //end question 10
+          // question 11
+    {
+        'answer' : 'c',
+        'question' : 'اشهر كوبرى فى مصر',
+        'options' : ['كوبري اكتوبر',
+        'كوبري اللمون',
+        'حسام غالى',
+        'كوبرى الفنجرى']
+    },
+    //end question 11
+          // question 12
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['',
+        '',
+        '',
+        '']
+    },
+    //end question 12
+          // question 13
+    {
+        'answer' : 'c',
+        'question' : 'ايه الحيوان المفضل لسيادة المستشار ؟',
+        'options' : ['النعامة',
+        'الخرارة',
+        'الديك',
+        'ام الخلول']
+    },
+    //end question 13
+          // question 14
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['',
+        '',
+        '',
+        '']
+    },
+    //end question 14
+          // question 15
+    {
+        'answer' : 'b',
+        'question' : 'من صاحب عبارة " العطشان يشرب من اي خرارة "',
+        'options' : ['المستشار',
+        'سيادة المستشار',
+        'مرتضي بيه',
+        'مورتا ( لو انت عايز سيديهاتك تطلع )']
+    },
+    //end question 15
+          // question 16
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['',
+        '',
+        '',
+        '']
+    },
+    //end question 16
+          // question 17
+    {
+        'answer' : 'b',
+        'question' : 'ايه هي الطويلة و عااااالية ؟',
+        'options' : ['غادة عبد الرازق',
+        'عرضية احمد فتحي',
+        'هيفاء وهبي',
+        'اى حاجة']
+    },
+    //end question 17
+          // question 18
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['','','','']
+    },
+    //end question 18
+          // question 19
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['','','','']
+    },
+    //end question 19
+          // question 20
+    {
+        'answer' : '',
+        'question' : '',
+        'options' : ['','','','']
+    },
+    //end question 20
+];
 
 
 });
